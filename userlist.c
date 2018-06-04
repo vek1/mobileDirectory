17df73f23abb59f3490f61a3ce27281c478ba66a
Shashi Kant Sharma <shashikant.sharma240@gmail.com>
2018-06-04 15:13:26 +0530

    function defined and header file created

--- userlist.c
+++ userlist.c
@@ -1,4 +1,13 @@
-char *user[5] = {"vivek", "shashi", "verma", "srivastav", "yogi"};
+#include <string.h>
+#define TUSERS 5
+
+char *user[TUSERS] = {"vivek", "shashi", "verma", "srivastav", "yogi"};
 int isUserPresent(const char *s){
-	
+	int i = 0;
+	while (i < TUSERS) {
+		if (!strcmp(*user[i], s))
+			return 1;
+		i++;
+	}
+	return 0;
 }
---
+++ userlist.h
@@ -0,0 +1,1 @@
+int isUserPresent(const char *s);
